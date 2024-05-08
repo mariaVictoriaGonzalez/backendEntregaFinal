c75ca802741dcc53df14f50528da0b7d6a25831a
Maria Victoria Gonzalez <mvg915@hotmail.com>
2024-05-07 22:36:57 -0300

    first commit

---
+++ src/routes/users.views.router.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.get("/premium/:uid", (req, res) => {
+  res.render("isItPremium")
+})
+
+router.get("/:uid", (req, res) => {
+  res.render("isItPremium")
+})
+
+export default router;
